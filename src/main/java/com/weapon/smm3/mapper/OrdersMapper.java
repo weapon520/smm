ebf07cc983477a96f2e9c8efdf80058fe6267b05
guoweipeng <guowp@pvc123.com>
2015-12-15 10:57:19 +0800

    sd

---
+++ src/main/java/com/weapon/smm3/mapper/OrdersMapper.java
@@ -0,0 +1,30 @@
+package com.weapon.smm3.mapper;
+
+import com.weapon.smm3.pojo.Orders;
+import com.weapon.smm3.pojo.OrdersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
